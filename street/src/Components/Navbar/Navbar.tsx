cc30238c601e2724c5425f0560d1a1f476dcb56a
TosuSW <denisappleid12@gmail.com>
2024-06-26 23:07:32 +0300

    Created Navbar component

---
+++ street/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+import { Link } from 'react-router-dom';
+
+import { useSession } from '../../States/Session/Session';
+
+
+import { FaDiscord } from "react-icons/fa";
+
+export default function Navbar() {
+
+    const { isSession } = useSession()
+
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.logo}>
+                <Link className='link' to='/'><img src='https://i.ibb.co/hFtwvZb/Street-Date-transparent.png'></img></Link>
+            </div>
+
+            <div className={styles.hrefs}>
+                <Link  className='link' to='/'><p>Startseite</p></Link>
+                <Link  className='link' to='/herunterladen'><p>Herunterladen</p></Link>
+                <Link className='link' to='/Über'><p>über</p></Link>
+                {isSession && <Link  className='link' to='/profilbereich'><p>Profilbereich</p></Link>}
+                {!isSession &&
+                    <>
+                        <Link  className='link' to='/registrierung'><p>Registrieren</p></Link>
+                        <Link className='link' to='/anmelden'> <p>Anmeldung</p></Link>
+                    </>
+                }
+            </div>
+
+            <div className={styles.discord}>
+                <span onClick={() => {window.open('https://discord.gg/YDWqmevJxk', '_blank')}}><FaDiscord /></span>
+            </div>
+        </div>
+    )
+}
