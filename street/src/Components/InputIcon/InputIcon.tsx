43ee499f2b9d5483fd116cdd24f07e96b8dbc097
TosuSW <denisappleid12@gmail.com>
2024-06-26 23:10:36 +0300

    Custom input component

---
+++ street/src/Components/InputIcon/InputIcon.tsx
@@ -0,0 +1,76 @@
+import { ReactNode, forwardRef, Ref, FormEventHandler } from 'react';
+import styles from './InputIcon.module.css';
+
+interface InputIconProps {
+    backColor: string;
+    width: string;
+    height: string;
+    borderRadius: string;
+    children: ReactNode;
+    color: string;
+    fontSize?: string;
+    iconBackColor?: string;
+    iconBorderRadius?: string;
+    title: string;
+    titleColor: string;
+    titleFontSize?: string;
+    type: string;
+    classN?: string;
+    border?: string;
+    iconFontSize?: string;
+    disabled?: boolean;
+    onInput?: FormEventHandler<HTMLInputElement>
+    onKeyDown?: FormEventHandler<HTMLInputElement>
+    onBlur?: FormEventHandler<HTMLInputElement>;
+    onFocus?: FormEventHandler<HTMLInputElement>;
+    onChange?: FormEventHandler<HTMLInputElement>;
+
+    value?: string
+    marginBottom?: string;
+}
+
+const InputIcon = forwardRef<HTMLInputElement, InputIconProps>(({
+    backColor, width, height, borderRadius, children, color,
+    fontSize, iconBackColor, iconBorderRadius,
+    title, titleColor,  type, classN, onInput,
+    value, marginBottom, onKeyDown, onBlur, onFocus, onChange, border, iconFontSize, disabled
+}: InputIconProps, ref: Ref<HTMLInputElement>) => {
+    return (
+        <div className={styles.inputicon}
+            style={{
+                backgroundColor: backColor,
+                width: width,
+                height: height,
+                borderRadius: borderRadius,
+                marginBottom: marginBottom,
+                border
+            }}>
+            <span className={styles.icon}
+                style={{
+                    color: color,
+                    backgroundColor: iconBackColor,
+                    borderRadius: iconBorderRadius,
+                    fontSize: iconFontSize,
+                }}>
+                {children}
+            </span>
+            <input disabled={disabled ? true : false} spellCheck='false' autoComplete='off' className={`${styles.theinput} ${classN}`}
+                name='InputIcon'
+                ref={ref}
+                type={type}
+                onInput={onInput}
+                onKeyDown={onKeyDown}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onChange={onChange}
+                placeholder={title}
+                value={value}
+                style={{
+                    color: titleColor,
+                    fontSize: fontSize,
+                }} />
+        </div>
+    );
+});
+
+export default InputIcon;
