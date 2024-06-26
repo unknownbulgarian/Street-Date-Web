15c86ddb8427763ec251391a2df702f408b65d06
TosuSW <denisappleid12@gmail.com>
2024-06-26 23:13:57 +0300

    Created login page guard

---
+++ street/src/Guards/LoginGuard/LoginGuard.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSession } from '../../States/Session/Session';
+import styles from '../ProtectedRoute.module.css'
+
+
+import { AiOutlineLoading } from "react-icons/ai";
+
+interface ProtectedRouteProps {
+    component: React.ComponentType<any>;
+}
+
+const LoginGuardRoute: React.FC<ProtectedRouteProps> = ({ component: Component }) => {
+    const { isSession, checkSession, isChecked } = useSession();
+
+    useEffect(() => {
+        const verifySession = async () => {
+            try {
+                await checkSession();
+                //setIsChecked(true);
+            } catch (error) {
+                console.error('Error checking session:', error);
+             //   setIsChecked(true); 
+            }
+        };
+
+        verifySession();
+    }, [checkSession]);
+
+    if (!isChecked) {
+        return <div className={styles.loader}><span className={styles.loadericon}><AiOutlineLoading /></span></div>;
+    }
+
+    return !isSession ? <Component /> : <Navigate to="/profilbereich" replace />;
+};
+
+export default LoginGuardRoute;
