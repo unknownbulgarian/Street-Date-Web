f09091f140b683f47af127d390c3806a95850c2c
TosuSW <denisappleid12@gmail.com>
2024-06-26 23:14:14 +0300

    Using the react router dom & added routing to login and register page

--- street/src/main.tsx
+++ street/src/main.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 import { AOSProvider } from './States/AOS/AOS.tsx'
 import { SessionProvider } from './States/Session/Session.tsx'
 import { OnlineProvider } from './States/Online/Online.tsx'
 
+//Pages
+import Register from './Pages/Register/Register.tsx'
+import Login from './Pages/Login/Login.tsx'
+
+//Guards
+import LoginGuardRoute from './Guards/LoginGuard/LoginGuard.tsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionProvider>
-      <OnlineProvider>
-        <AOSProvider>
-          <App />
-        </AOSProvider>
-      </OnlineProvider>
-    </SessionProvider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <SessionProvider>
+        <OnlineProvider>
+          <AOSProvider>
+            <Routes>
+              <Route path='/' element={<App />} />
+              <Route path='/registrierung' element={<LoginGuardRoute component={Register} />} />
+              <Route path='/anmelden' element={<LoginGuardRoute component={Login} />} />
+            </Routes>
+          </AOSProvider>
+        </OnlineProvider>
+      </SessionProvider>
+    </BrowserRouter>
+  </React.StrictMode >,
 )
