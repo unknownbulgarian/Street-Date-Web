65803a7507f6ef10794e0e0a34d46ec0e4ad877a
TosuSW <denisappleid12@gmail.com>
2024-07-03 14:55:54 +0300

    Update TextIcon.tsx

--- street/src/Components/TextIcon/TextIcon.tsx
+++ street/src/Components/TextIcon/TextIcon.tsx
@@ -20,6 +20,7 @@ interface BoxIconProps {
     iconFontSize?: string;
     titleColor?: string;
     nav?: boolean;
+    boxShadow?: string;
     marginTop?: string;
     textShadow?: string;
     onClick?: MouseEventHandler<HTMLDivElement>;
@@ -30,10 +31,10 @@ interface BoxIconProps {
 export default function TextIcon({ width, height, borderRadius, backgroundColor,
     title, children, border, titleFontWeight, padding, 
     paddingLeft, paddingRight, textTransform, nav, transition, color, fontSize, iconFontSize,
-    onClick, marginTop, onHover, onUnHover, titleColor, textShadow }: BoxIconProps) {
+    onClick, marginTop, onHover, onUnHover, titleColor, textShadow,boxShadow }: BoxIconProps) {
     return (
         <div onClick={onClick} onMouseOver={onHover} onMouseLeave={onUnHover} style={{ width, height, borderRadius, backgroundColor, 
-            border, paddingLeft, paddingRight, transition, color, fontSize, padding, marginTop }} 
+            border, paddingLeft, paddingRight, transition, color, fontSize, padding, marginTop, boxShadow }} 
         className=
         {`${nav ? `${styles.box} ${styles.navhover}` : `${styles.box} ${styles.default}` }`}>
                <span style={{fontSize: iconFontSize}} className={styles.icon}>{children}</span>
