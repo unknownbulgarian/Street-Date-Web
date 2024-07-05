0f0861cc654c30e5efa770a3d113b487bb123161
TosuSW <denisappleid12@gmail.com>
2024-07-05 14:53:24 +0300

    Explore component created

---
+++ street/src/Pages/Explore/Explore.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+import Navbar from '../../Components/Navbar/Navbar'
+
+export default function Explore() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
