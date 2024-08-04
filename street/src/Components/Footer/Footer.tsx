dd2247e22884a339e915c38c572b03528b90de80
TosuSW <denisappleid12@gmail.com>
2024-08-04 11:58:31 +0300

    Footer rework
    - new back color
    - rating element

--- street/src/Components/Footer/Footer.tsx
+++ street/src/Components/Footer/Footer.tsx
@@ -3,47 +3,64 @@ import styles from './Footer.module.css'
 
 import { Link } from 'react-router-dom'
 
+import { IoStarSharp } from "react-icons/io5";
+
 export default function Footer() {
     return (
-        <div className={styles.footer}>
-            <div className={styles.footerboxes}>
-
-                <div onClick={() => { location.href = '/' }} className={styles.name}>
-                    <img src='https://i.postimg.cc/SKH1Dy1G/Screenshot-6-removebg-preview.png'></img>
-                    <p>StreetDate</p>
+        <>
+            <div className={styles.main}>
+                <div className={styles.review}>
+                    <h2><span style={{ textDecoration: 'underline' }}>Online</span> StreetDate</h2>
+                    <div className={styles.stars}>
+                        <IoStarSharp />
+                        <IoStarSharp />
+                        <IoStarSharp />
+                        <IoStarSharp />
+                        <IoStarSharp />
+                        <IoStarSharp />
+                    </div>
                 </div>
+                <div className={styles.footer}>
+                    <div className={styles.footerboxes}>
 
-                <div className={styles.footerbox}>
-                    <h2>Allgemein</h2>
-                    <Link className='link' to='/'><p>Startseite</p></Link>
-                    <Link className='link' to='/über'><p>Über</p></Link>
-                </div>
+                        <div onClick={() => { location.href = '/' }} className={styles.name}>
+                            <img src='https://i.postimg.cc/SKH1Dy1G/Screenshot-6-removebg-preview.png'></img>
+                            <p>StreetDate</p>
+                        </div>
 
-                <div className={styles.footerbox}>
-                    <h2>Die App</h2>
-                    <Link className='link' to='/herunterladen'><p>Herunterladen</p></Link>
-                    <p onClick={() => { window.open('', '_blank') }}>Für Android</p>
-                    <p onClick={() => { window.open('', '_blank') }}>Für IOS</p>
-                    <Link className='link' to='/versuchen'><p>Als Gast versuchen</p></Link>
-                    <p onClick={() => { window.open('https://discord.gg/YDWqmevJxk', '_blank') }}>Discord Server</p>
-                </div>
+                        <div className={styles.footerbox}>
+                            <h2>Allgemein</h2>
+                            <Link className='link' to='/'><p>Startseite</p></Link>
+                            <Link className='link' to='/über'><p>Über</p></Link>
+                        </div>
 
-                <div className={styles.footerbox}>
-                    <h2>Konto</h2>
-                    <Link className='link' to='/registrierung'><p>Registrierung</p></Link>
-                    <Link className='link' to='/anmelden'><p>Anmelden</p></Link>
-                    <Link className='link' to='/profilbereich'><p>Profilbereich</p></Link>
-                </div>
+                        <div className={styles.footerbox}>
+                            <h2>Die App</h2>
+                            <Link className='link' to='/herunterladen'><p>Herunterladen</p></Link>
+                            <p onClick={() => { window.open('', '_blank') }}>Für Android</p>
+                            <p onClick={() => { window.open('', '_blank') }}>Für IOS</p>
+                            <Link className='link' to='/versuchen'><p>Als Gast versuchen</p></Link>
+                            <p onClick={() => { window.open('https://discord.gg/YDWqmevJxk', '_blank') }}>Discord Server</p>
+                        </div>
 
-                <div className={styles.footerbox}>
-                    <h2>Unbedingt lesen</h2>
-                    <Link className='link' to='/bedingungen und konditionen'><p>Bedingungen und Konditionen</p></Link>
-                    <Link className='link' to='/politiken'><p>Politiken</p></Link>
-                    <Link className='link' to='/regeln'><p>Regeln</p></Link>
+                        <div className={styles.footerbox}>
+                            <h2>Konto</h2>
+                            <Link className='link' to='/registrierung'><p>Registrierung</p></Link>
+                            <Link className='link' to='/anmelden'><p>Anmelden</p></Link>
+                            <Link className='link' to='/profilbereich'><p>Profilbereich</p></Link>
+                        </div>
+
+                        <div className={styles.footerbox}>
+                            <h2>Unbedingt lesen</h2>
+                            <Link className='link' to='/bedingungen und konditionen'><p>Bedingungen und Konditionen</p></Link>
+                            <Link className='link' to='/politiken'><p>Politiken</p></Link>
+                            <Link className='link' to='/regeln'><p>Regeln</p></Link>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
-
-
-        </div>
+        </>
     )
 }
